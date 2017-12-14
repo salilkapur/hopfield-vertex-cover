e3197d1ce6c9349ff90278e3c88b61f682e96b61
Steph Carrera <sc2936@nyu.edu>
2017-12-14 18:38:22 -0500

    testing adding 2 functions to mvc

--- mvc.c
+++ mvc.c
@@ -1,12 +1,32 @@
 #include "common.h"
 #include "graph.h"
 #include "io_utility.h"
+#include "math.h"
+#include "hnn.h"
 
-#define rounds 500;
+#define rounds 500
 
 //Declared in common.h
 char masks[8] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
 int results[rounds];
+
+//error
+int minVC(struct graph* G){
+    int min = G->N;
+    for( int i = 0; i < rounds; i++){
+      if(results[i] < min){
+        min = results[i];
+      }
+    }
+    return min;
+}
+
+float outputPotential(struct model* model, int i){
+    //float x = model->weights[i] / model->hyperparameters->theta;
+    float x = model->weights[i] / model->h_params->theta;
+    float v = 1/(1 + expf(-x));
+    return v;
+}
 
 
 int main(int argc, char** argv) {
@@ -26,20 +46,6 @@ int main(int argc, char** argv) {
     return 1;
 }
 
-int minVC(graph G){
-    int min = G.N;
-    for( int i = 0; i < rounds; i++){
-      if(results[i] < min){
-        min = results[i];
-      }
-    }
-    return min;
-}
 
-float outputPotential(int i){
-    float x = model->weights[i] / model->hyperparameters->theta;
-    float v = 1/(1 + expf(-x));
-    return v;
-}
 
 
